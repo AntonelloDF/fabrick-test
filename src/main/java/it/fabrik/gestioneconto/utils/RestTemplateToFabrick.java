1c0118f62a485c11a73c9f508f3194e98e43aa7b
AntonelloDF <a.difranco@inno-tek.it>
2022-11-18 15:09:48 +0100

    fix restTemplate

---
+++ src/main/java/it/fabrik/gestioneconto/utils/RestTemplateToFabrick.java
@@ -0,0 +1,9 @@
+package it.fabrik.gestioneconto.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateToFabrick  extends RestTemplate{
+
+}
